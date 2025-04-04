c4e98a943755f3b124fb80e07e7ab56f27e8f967
pichu1298 <62480135+pichu1298@users.noreply.github.com>
2025-04-04 12:32:33 -0400

    Create GachaSystem.vue

---
+++ app/src/components/GachaSystem.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <button type="submit" @click="pullTen">Pull 10X!</button>
+  </div>
+</template>
+
+<script setup>
+let {data}
+function pullTen() {}
+</script>
+
+<style scoped></style>
