93e1e54b310ff702e1f603dc551e84032de44a66
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2025-04-04 12:32:35 -0400

    sfdfdssfd

---
+++ app/src/Cards.ts
@@ -0,0 +1,12 @@
+export type card = {
+    name: string;
+    description: string;
+    power: number;
+    leader: boolean;
+    card_type: string;
+    card_image: string;
+
+}
+
+
+export const decks: card[] = []
