0126a032c7e31a2ca854c3b08d75f7fdc5f31aaa
pichu1298 <62480135+pichu1298@users.noreply.github.com>
2025-04-08 12:28:06 -0400

    fixing gacha system rates

--- app/src/components/GachaSystem.vue
+++ app/src/components/GachaSystem.vue
@@ -8,15 +8,28 @@
 <script setup lang="ts">
 //supposed to pull stuff from supabase
 let { cards: allCards, error } = await supabase.from('allCards')
+let pityCount = 0
 let pulledCards: string[] = [] //array for cards that were pulled
 function pullTen() {
   for (let i = 0; i < 10; i++) {
-    pulledCards.push(cards[Math.floor(Math.random() * allCards.length + 1)])
+    pityCount++
+    if (pityCount >= 90) {
+      //pull five star card!!!
+      pityCount = 0
+    } else {
+      pulledCards.push(cards[Math.floor(Math.random() * allCards.length + 1)])
+    }
   }
   console.log(pulledCards) //basically pull 10 random cards and show in console.log
 }
 function pullOne() {
-  pulledCards.push(cards[Math.floor(Math.random() * allCards.length + 1)])
+  pityCount++
+  if (pityCount >= 90) {
+    //pull five star card!!!
+    pityCount = 0
+  } else {
+    pulledCards.push(cards[Math.floor(Math.random() * allCards.length + 1)])
+  }
   console.log(pulledCards) //pull one card and show it
 }
 </script>
