4326a649f0ad61200cd7df08028d15fff22c3a55
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2025-04-22 12:32:12 -0400

    sdfdssdf

--- app/src/firebase.ts
+++ app/src/firebase.ts
@@ -1,22 +1,28 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your Firebase configuration object
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCnpc5bfZ2O1e20vw8-qHRbYcbkr-UPeyg",
+  authDomain: "super-trash-gambling-raccoons.firebaseapp.com",
+  projectId: "super-trash-gambling-raccoons",
+  storageBucket: "super-trash-gambling-raccoons.firebasestorage.app",
+  messagingSenderId: "1073399694818",
+  appId: "1:1073399694818:web:f359c231cadebdbf18e5bb",
+  measurementId: "G-40VMRQZXJG"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-// Export Firebase services
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { app, auth, db };
+export { auth, db };
