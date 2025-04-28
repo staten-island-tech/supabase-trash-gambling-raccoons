01bad34c2d11ce5887ca2fc05d55d0e8ea9978ee
pichu1298 <62480135+pichu1298@users.noreply.github.com>
2025-04-28 12:11:55 -0400

    stuff

---
+++ app/src/components/deckStuff/CardsInDeck.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Cards Down HERE!</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { type card } from '../ts/types'
+type Props = {
+  deckItems: card
+}
+defineProps<Props>()
+</script>
+
+<style lang="scss" scoped></style>
--- app/src/components/deckStuff/DeckCards.vue
+++ app/src/components/deckStuff/DeckCards.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="deck">
-    <button type="submit" @click="showCards">{{ deckItems.name }}</button>
+    <button type="submit" @click="isClicked = !isClicked">{{ deckItems.name }}</button>
+    <CardsInDeck v-show="isClicked" v-for="card in deckItems.cards" :deckItems="card" />
   </div>
 </template>
 
 <script setup lang="ts">
 import type { deck } from '../ts/types'
+import CardsInDeck from './CardsInDeck.vue'
+import { ref } from 'vue'
+
+const isClicked = ref(true)
+
 type Props = {
   deckItems: deck
 }
 defineProps<Props>()
-
-function showCards() {}
 </script>
 
 <style scoped></style>
