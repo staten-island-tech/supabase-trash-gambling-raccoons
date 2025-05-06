eec7074a9f3234d8fa6913f88cb50b94da761c6f
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2025-05-06 12:32:13 -0400

    fdsfdssfd

--- app/src/userdocument.ts
+++ app/src/userdocument.ts
@@ -1,19 +1,40 @@
 import { doc, updateDoc } from "firebase/firestore"
 import { db } from "../firebase"
 
-const UserArrays = async (uid: string) => {
+const adddecks = async (uid: string, searchitem) => {
     try {
         const userDocRef = doc(db, "users", uid);
         /* reference user doc */
 
-        /* await changedoc(userDocRef, {
+         await changedoc(userDocRef, {
             profile: {
-                email: ,
-                name: ,
+                email: "user@example.com",
+                name: "User Name",
             },
             inventory: {
                 decks: []
             }
-        }) */
+        }) 
     }
 }
+
+/* 
+users/
+    decks: [
+    {name: name,
+     cards: card[]},
+    {}
+    ]
+    
+
+
+    battlehistory:[
+    {battleID: "Battle 1",
+    battledate: "05-05-2025"
+    },
+    
+    ]
+
+
+
+*/
