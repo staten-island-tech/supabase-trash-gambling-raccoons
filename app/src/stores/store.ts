4d5b0ea844b58b7c46323b9ea6424559c95eab86
haobinjhin <144480283+haobinjhin@users.noreply.github.com>
2025-06-12 08:15:24 -0400

    stuff

--- app/src/stores/store.ts
+++ app/src/stores/store.ts
@@ -1,5 +1,8 @@
 import { reactive } from "vue";
 
-export const 
+export const AllowedorNotallowed = reactive({
+  allowedViews:['Login', 'Home'],
+  RestrictedViews:[],
+  )}
 
  /* look at chatgpt trust REMEMBER */
